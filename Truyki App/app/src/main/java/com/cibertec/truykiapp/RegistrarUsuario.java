5b9976ae9850463cb553f00bc64300c540cb7e37
paulable <carlospgp13@gmail.com>
2020-11-09 11:39:25 -0500

    Layout Formulario de registro v1

---
+++ Truyki App/app/src/main/java/com/cibertec/truykiapp/RegistrarUsuario.java
@@ -0,0 +1,14 @@
+package com.cibertec.truykiapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegistrarUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrar_usuario);
+    }
+}
