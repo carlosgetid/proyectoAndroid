562ca3f3565cf729374ea27879e8e7c0a133a064
paulable <carlospgp13@gmail.com>
2020-11-22 18:11:35 -0500

    avance de Favio

--- Truyki App/app/src/main/java/com/cibertec/truykiapp/RegistrarUsuario.java
+++ Truyki App/app/src/main/java/com/cibertec/truykiapp/RegistrarUsuario.java
@@ -9,6 +9,6 @@ public class RegistrarUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrar_usuario);
+        setContentView(R.layout.login);
     }
 }
